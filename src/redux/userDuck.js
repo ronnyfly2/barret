89e97a1a83d5d083323a81447294c4a5dec41ca5
Ronny <ronny_the_fly7@hotmail.com>
2020-06-04 11:33:44 -0500

    chore: agregando y configurando herramientas

---
+++ src/redux/userDuck.js
@@ -0,0 +1,12 @@
+let initialData = {
+	loggedIn:false
+}
+const LOGIN = 'LOGIN';
+
+export default function reducer(state = initialData, action){
+	switch(action.type){
+		case LOGIN:
+		default:
+			return state
+	}
+}
